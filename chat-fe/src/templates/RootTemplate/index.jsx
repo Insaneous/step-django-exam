d9939be1512ce3274a7f09314953e8c029961342
insaneous <insaneous@proton.me>
2025-05-31 17:53:01 +0500

    docker

--- chat-fe/src/templates/RootTemplate/index.jsx
+++ chat-fe/src/templates/RootTemplate/index.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../../redux/slice/authSlice";
 import { getCurrentUser } from "../../api/apiClient";
 import { Outlet, useNavigate } from "react-router-dom";
-import styles from './style.module.css';
 
 export const RootTemplate = () => {
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
@@ -19,21 +17,17 @@ export const RootTemplate = () => {
         .then((response) => {
           const user = response.data;
           dispatch(setUser({ user, token: storedToken }));
-          setLoading(false);
         })
         .catch((error) => {
           console.error("Authentication failed", error);
           navigate("/auth");
         });
     } else {
-      setLoading(false);
       navigate("/auth");
     }
   }, [dispatch, navigate, token]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+
 
   return (
     <main>
